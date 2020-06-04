476f478926e61886f58d34909cf558019f2159bd
Paulo Rubens <paulojrrubens@gmail.com>
2020-06-04 15:20:32 -0300

    Finished back-end and front-end

--- aulas/server/src/server.ts
+++ aulas/server/src/server.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import path from 'path';
+import routes from './routes';
+import cors from 'cors';
+
+//Request Param: Parâmetros que vem na própria rota que identificam um recurso
+//Queru Param: Parâmetros que vem na própria rota geralmenteopcionais para filtros, paginação
+//Request Body: Parâmetros para criação/atualização de informações 
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    response.send('Hello World');
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
